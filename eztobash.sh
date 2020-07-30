5d4dd4669b1bc02648d7e2dc7139e618fd7f6179
Qiraht <48444430+Qiraht@users.noreply.github.com>
2020-07-30 23:28:32 +0700

    Update

--- eztobash.sh
+++ eztobash.sh
@@ -4,6 +4,6 @@ read name
 git config --global user.name "$name"
 echo -n "Masukkan Email :"
 read email
-git config --global "$email"
+git config --global user.email "$email"
 clear
 git config --list
